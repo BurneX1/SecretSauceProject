c37e8bc6d7df800bc2378d9310339b5160031f9f
Alejandro <almpratto@gmail.com>
2021-07-05 13:53:44 -0500

    Cambio de nombre de escenas y avance de opcionesde sonido

---
+++ Assets/Scripts/General/Opciones.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Opciones : MonoBehaviour
+{
+
+    [Header("ShowOnly Volumes")]
+    public float genVolume;
+    public float mscVolume;
+    public float sndVolume;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("GenVol") == false)
+        {
+            PlayerPrefs.SetFloat("GenVol", 0.5f);
+        }
+        if (PlayerPrefs.HasKey("MscVol") == false)
+        {
+            PlayerPrefs.SetFloat("MscVol", 0.5f);
+        }
+        if (PlayerPrefs.HasKey("SndVol") == false)
+        {
+            PlayerPrefs.SetFloat("SndVol", 0.5f);
+        }
+
+        genVolume = PlayerPrefs.GetFloat("GenVol");
+        mscVolume = PlayerPrefs.GetFloat("MscVol");
+        sndVolume = PlayerPrefs.GetFloat("SndVol");
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetGenVolume(float vol)
+    {
+        PlayerPrefs.SetFloat("GenVol", vol);
+        genVolume = PlayerPrefs.GetFloat("GenVol");
+
+        
+    }
+    public void SetSndVolume(float vol)
+    {
+        sndVolume = PlayerPrefs.GetFloat("SndVol");
+        PlayerPrefs.SetFloat("SndVol", vol);
+
+    }
+    public void SetMscVolume(float vol)
+    {
+        PlayerPrefs.SetFloat("MscVol", vol);
+        mscVolume = PlayerPrefs.GetFloat("MscVol");
+
+
+    }
+}
