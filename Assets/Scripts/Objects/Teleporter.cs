7cece95b10512ae523be2a8f84ae46f61de7a3bb
Alejandro <almpratto@gmail.com>
2021-07-05 06:40:24 -0500

    Armado de Primera Planta

---
+++ Assets/Scripts/Objects/Teleporter.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Teleporter : InteractableAction
+{
+    public Transform tpPosition;
+    public GameObject tpObj;
+    public bool yAxis;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void Activation()
+    {
+
+        if (yAxis == false)
+        {
+            if(tpObj.GetComponent<NavMeshAgent>())
+            {
+                tpObj.GetComponent<NavMeshAgent>().enabled = false;
+                tpObj.transform.position = new Vector3(tpPosition.position.x, tpObj.transform.position.y, tpPosition.position.z);
+                tpObj.GetComponent<NavMeshAgent>().enabled = true;
+            }
+            else
+            {
+                tpObj.transform.position = new Vector3(tpPosition.position.x, tpObj.transform.position.y, tpPosition.position.z);
+            }
+            
+        }
+        else
+        {
+            if (tpObj.GetComponent<NavMeshAgent>())
+            {
+                tpObj.GetComponent<NavMeshAgent>().enabled = false;
+                tpObj.transform.position = tpPosition.position;
+                tpObj.GetComponent<NavMeshAgent>().enabled = true;
+            }
+            else
+            {
+                tpObj.transform.position = tpPosition.position;
+            }
+        
+        }
+    }
+}
