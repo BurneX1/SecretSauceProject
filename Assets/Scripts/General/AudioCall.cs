77671c4cf8758819e7ca129f1d640cc5a939ab92
Alejandro Medina <almpratto@gmail.com>
2021-07-08 11:21:41 -0500

    Something

---
+++ Assets/Scripts/General/AudioCall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CallAudio(string audioName)
+    {
+        if (GameObject.Find("SoundManager").GetComponent<AudioManager>())
+        {
+            GameObject.Find("SoundManager").GetComponent<AudioManager>().Play(audioName);
+        }
+    }
+}
