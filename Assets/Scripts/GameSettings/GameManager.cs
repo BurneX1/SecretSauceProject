ac1e19ae81e8ba77fa84272ff9211e181ebb3f25
Alejandro Medina <almpratto@gmail.com>
2021-09-17 07:03:34 -0500

    GameManager with devMode option

---
+++ Assets/Scripts/GameSettings/GameManager.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region enumLabels
+    public enum gameMode
+    {
+        UserMode = 0,
+
+        DeveloperMode = 1,
+
+        EditorMode = 2
+    }
+    public enum conectionState
+    {
+        Offline = 0,
+
+        Online = 1,
+
+        Conecting = 2
+    }
+    public enum gameState
+    {
+        MenuStaying = 0,
+
+        Playing = 1,
+
+        Win = 2,
+
+        Lose = 2,
+    }
+    #endregion
+
+    [SerializeField] protected gameMode m_GameMode = gameMode.UserMode;
+    [SerializeField] protected conectionState m_Conection = conectionState.Offline;
+    [SerializeField] protected gameState m_State = gameState.MenuStaying;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
