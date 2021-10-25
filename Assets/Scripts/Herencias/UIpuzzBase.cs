31e79428920753da5c2fd506caac456af954043c
Alejandro Medina <almpratto@gmail.com>
2021-10-25 11:22:11 -0500

    Base para puzzles interactuables

---
+++ Assets/Scripts/Herencias/UIpuzzBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UIpuzzBase : MonoBehaviour
+{
+    public GameObject plyCtrll;
+    private Pause ps;
+    public KeyCode[] keysToUse;
+    public UnityEvent onFinishEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = plyCtrll.GetComponent<Pause>();
+
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && ps.paused == false)
+        {
+            ClosePuzzle();
+            
+        }
+    }
+
+    public void ClosePuzzle()
+    {
+        Time.timeScale = 1;
+        gameObject.SetActive(false);
+    }
+
+    public void FinishPuzzle()
+    {
+
+        onFinishEvent.Invoke();
+        ClosePuzzle();
+    }
+}
